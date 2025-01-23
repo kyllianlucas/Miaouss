9a3066e1e3434defa9209cbacd93c0572f92d299
Ayoub E <a.ech-charrat@1-one.fr>
2025-01-23 16:40:14 +0100

    add CRUD messages and posts ; add .env var and db.js setup (2)

--- src/db.js
+++ src/db.js
@@ -1,11 +1,16 @@
 import { Account, Client, Databases, Storage, ID } from "appwrite";
 
-export const DATABASE_ID = import.meta.env.DATABASE_ID;
-export const USERS_COLLECTION_ID = import.meta.env.USERS_COLLECTION_ID;
-export const POSTS_COLLECTION_ID = import.meta.env.POSTS_COLLECTION_ID;
+export const DATABASE_ID = import.meta.env.VITE_DATABASE_ID;
+export const USERS_COLLECTION_ID = import.meta.env.VITE_USERS_COLLECTION_ID;
+export const POSTS_COLLECTION_ID = import.meta.env.VITE_POSTS_COLLECTION_ID;
+export const MESSAGES_COLLECTION_ID = import.meta.env
+  .VITE_MESSAGES_COLLECTION_ID;
+export const LIKES_COLLECTION_ID = import.meta.env.VITE_LIKES_COLLECTION_ID;
 
 export const client = new Client();
-client.setEndpoint("http://localhost/v1").setProject("6790adfd0030e6953858");
+client
+  .setEndpoint("http://172.27.74.113:7777/v1")
+  .setProject("6792466000195ecf0f8f");
 
 export const account = new Account(client);
 export const database = new Databases(client);
---
+++ src/services/postsServices.jsx
@@ -0,0 +1,91 @@
+import {
+  client,
+  database,
+  DATABASE_ID,
+  POSTS_COLLECTION_ID,
+} from "../../../Littler/src/db";
+import { Query } from "appwrite";
+
+export const postsServices = {
+  getAll: async () => {
+    return await database.listDocuments(DATABASE_ID, POSTS_COLLECTION_ID);
+  },
+  listen: (setPosts) => {
+    return client.subscribe(
+      [`databases.${DATABASE_ID}.collections.${POSTS_COLLECTION_ID}.documents`],
+      (res) => {
+        if (
+          res.events.includes("databases.*.collections.*.documents.*.create")
+        ) {
+          setPosts((prev) => [...prev, res.payload]);
+          console.log("element cree :", res.payload);
+        }
+        if (
+          res.events.includes("databases.*.collections.*.documents.*.delete")
+        ) {
+          setPosts((prev) =>
+            prev.filter((post) => post.$id !== res.payload.$id)
+          );
+          console.log("element supprime :", res.payload);
+        }
+        if (
+          res.events.includes("databases.*.collections.*.documents.*.update")
+        ) {
+          console.log("element modifie :", res.payload);
+        }
+      }
+    );
+  },
+  post: async (postJSON) => {
+    return await database.createDocument(
+      DATABASE_ID,
+      POSTS_COLLECTION_ID,
+      getUniqueId(),
+      postJSON
+    );
+  },
+  delete: async (postId) => {
+    return await database.deleteDocument(
+      DATABASE_ID,
+      POSTS_COLLECTION_ID,
+      postId
+    );
+  },
+  modify: async (postId, updates) => {
+    return await database.updateDocument(
+      DATABASE_ID,
+      POSTS_COLLECTION_ID,
+      postId,
+      updates
+    );
+  },
+  getAllFromId: async (userId) => {
+    return await database.listDocuments(
+      DATABASE_ID,
+      POSTS_COLLECTION_ID,
+      Query.equal("userId", userId)
+    );
+  },
+  putALike: async (userId, postId) => {
+    const isLikedByUser = await database.getDocument(
+      DATABASE_ID,
+      LIKES_COLLECTION_ID,
+      Query.and([
+        Query.equal("collectionId", postId),
+        Query.equal("userId", userId),
+      ])
+    );
+    if (isLikedByUser !== null) {
+      const sendLike = await database.createDocument(
+        DATABASE_ID,
+        LIKES_COLLECTION_ID,
+        Query.and([
+          Query.equal("collectionId", postId),
+          Query.equal("userId", userId),
+        ])
+      );
+      return sendLike;
+    }
+    return;
+  },
+};
