38dfcb24ef2919d3253555511f4f93f3d7d43716
jessy fanguimache <jessy.fangui@gmail.com>
2025-01-24 12:29:44 +0100

    dev post

---
+++ src/components/ui/inputPostStyle.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Input } from "./input";
+import { Button } from "./button";
+
+function InputPostStyle() {
+  return <></>;
+}
+
+export default InputPostStyle;
---
+++ src/pages/postMessage.jsx
@@ -0,0 +1,12 @@
+import InputPostContainer from "@/components/ui/inputPostContainer";
+import React from "react";
+
+function postMessage() {
+  return (
+    <>
+      <InputPostContainer />
+    </>
+  );
+}
+
+export default postMessage;
