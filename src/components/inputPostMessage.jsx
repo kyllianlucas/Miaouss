78795f1c7c402f5541dbeb72f616e02c27de055f
jessy fanguimache <jessy.fangui@gmail.com>
2025-01-24 15:40:20 +0100

    merge conflits

--- src/components/inputPostMessage.jsx
+++ src/components/inputPostMessage.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
+import { useState } from "react";
 
 function InputPostMessage() {
+  const [text, setText] = useState("");
+
   return (
     <>
       <div className="grid grid-cols-6 gap-5 w-full items-center justify-center">
         <div className="col-span-5">
-          <Textarea type="textarea" placeholder="Rédigez votre post..." />
+          <Textarea
+            type="textarea"
+            placeholder="Rédigez votre post..."
+            rows="7"
+          />
         </div>
-        <Button>Envokjjjjyer</Button>
+        <Button>Envoyer</Button>
       </div>
     </>
   );
